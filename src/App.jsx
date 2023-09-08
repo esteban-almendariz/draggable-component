c3095e095b50ae95ad3629b5fda153a4713f0104
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2023-09-07 23:15:52 -0500

    re creating draggable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,107 @@
 import { useState, useEffect } from 'react'
-import CreateTask from './components/CreateTask'
-import ListTasks from './components/ListTasks'
-import toast, { Toaster } from 'react-hot-toast';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import { v4 as uuidv4 } from 'uuid'
 
+const itemsFromBackend = [
+  { id: uuidv4(), content: 'First Task'},
+  { id: uuidv4(), content: 'Second Task'}
+]
+
+const columnsFromBackend = {
+  
+    [uuidv4()]: {
+      name: 'Todo',
+      items: itemsFromBackend
+    },
+    [uuidv4()]: {
+      name: 'In Progress',
+      items: []
+    }
+  
+}
+
+const onDragEnd = (result, columns, setColumns) => {
+  if(!result.destination) return
+  const {source, destination} = result
+  const column = columns[source.droppableId]
+  const copiedItems = [...column.items]
+  const [removed] = copiedItems.splice(source.index, 1)
+  copiedItems.splice(destination.index, 0 , removed)
+  setColumns({
+    ...columns,
+    [source.droppableId]: {
+      ...column,
+      items: copiedItems
+    }
+  })
+}
 
 function App() {
-  const [tasks, setTasks] = useState([])
-
-  console.log('Tasks from App: ', tasks)
-
-  useEffect(() => {
-    setTasks(JSON.parse(localStorage.getItem('tasks')))
-  }, [])
+  
+  const [columns, setColumns] = useState(columnsFromBackend)
 
   return (
-    <>
-      <Toaster />
-      <div className='bg-slate-100 w-screen h-screen flex flex-col items-center pt-3 gap-16 pt-32'>
-        <CreateTask tasks={tasks} setTasks={setTasks}/>
-        <ListTasks tasks={tasks} setTasks={setTasks} />
-      </div>
-    </>
+    <div style={{ display: 'flex', justifyContent: 'center', height: '100%'}}>
+      <DragDropContext onDragEnd= {result => onDragEnd(result, columns, setColumns)}>
+        {/* Each droppable will have its own key */}
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <div style={{display:'flex', flexDirection:'column', alignContent:'center'}}>
+              <h2>{column.name}</h2>
+              <Droppable key={id} droppableId={id}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                        padding: 4,
+                        width: 250,
+                        minHeight: 500
+                      }}
+                    >
+                      {column.items.map((item, index) => {
+                        return (
+                          <Draggable key={item.id} draggableId={item.id} index={index}>
+                            {(provided, snapshot) => {
+                              return (
+                                <div 
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  style={{
+                                    userSelect: 'none',
+                                    padding: 16,
+                                    margin: '0 0 8px 0',
+                                    minHeight: '50px',
+                                    backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
+                                    color: 'white',
+                                    ...provided.draggableProps.style
+                                  }}
+                                >
+                                  {item.content}
+                                </div>
+                              )
+                            }}
+
+                          </Draggable>
+                        )
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  )
+                }}
+              </Droppable>
+            </div>
+          )
+        })}
+      </DragDropContext>
+    </div>
   )
+
+
+
 }
 
 export default App
