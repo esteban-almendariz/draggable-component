14519476a909f6d315542560b465114ad8f63bc7
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2023-09-12 17:47:06 -0500

    finished draggable component

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,26 @@ const columnsFromBackend = {
 const onDragEnd = (result, columns, setColumns) => {
   if(!result.destination) return
   const {source, destination} = result
-  const column = columns[source.droppableId]
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    });
+  } else {
+    const column = columns[source.droppableId]
   const copiedItems = [...column.items]
   const [removed] = copiedItems.splice(source.index, 1)
   copiedItems.splice(destination.index, 0 , removed)
@@ -34,6 +53,8 @@ const onDragEnd = (result, columns, setColumns) => {
       items: copiedItems
     }
   })
+  }
+  
 }
 
 function App() {
@@ -46,53 +67,55 @@ function App() {
         {/* Each droppable will have its own key */}
         {Object.entries(columns).map(([id, column]) => {
           return (
-            <div style={{display:'flex', flexDirection:'column', alignContent:'center'}}>
+            <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
               <h2>{column.name}</h2>
-              <Droppable key={id} droppableId={id}>
-                {(provided, snapshot) => {
-                  return (
-                    <div
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                      style={{
-                        background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
-                        padding: 4,
-                        width: 250,
-                        minHeight: 500
-                      }}
-                    >
-                      {column.items.map((item, index) => {
-                        return (
-                          <Draggable key={item.id} draggableId={item.id} index={index}>
-                            {(provided, snapshot) => {
-                              return (
-                                <div 
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                  {...provided.dragHandleProps}
-                                  style={{
-                                    userSelect: 'none',
-                                    padding: 16,
-                                    margin: '0 0 8px 0',
-                                    minHeight: '50px',
-                                    backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
-                                    color: 'white',
-                                    ...provided.draggableProps.style
-                                  }}
-                                >
-                                  {item.content}
-                                </div>
-                              )
-                            }}
+              <div style={{margin: 8}}>
+                <Droppable key={id} droppableId={id}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{
+                          background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                          padding: 4,
+                          width: 250,
+                          minHeight: 500
+                        }}
+                      >
+                        {column.items.map((item, index) => {
+                          return (
+                            <Draggable key={item.id} draggableId={item.id} index={index}>
+                              {(provided, snapshot) => {
+                                return (
+                                  <div 
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    style={{
+                                      userSelect: 'none',
+                                      padding: 16,
+                                      margin: '0 0 8px 0',
+                                      minHeight: '50px',
+                                      backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
+                                      color: 'white',
+                                      ...provided.draggableProps.style
+                                    }}
+                                  >
+                                    {item.content}
+                                  </div>
+                                )
+                              }}
 
-                          </Draggable>
-                        )
-                      })}
-                      {provided.placeholder}
-                    </div>
-                  )
-                }}
-              </Droppable>
+                            </Draggable>
+                          )
+                        })}
+                        {provided.placeholder}
+                      </div>
+                    )
+                  }}
+                </Droppable>
+              </div>
             </div>
           )
         })}
@@ -100,7 +123,7 @@ function App() {
     </div>
   )
 
-
+// source: https://www.youtube.com/watch?v=Vqa9NMzF3wc&t=541s
 
 }
 
